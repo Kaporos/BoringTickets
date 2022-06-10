58184e4561cca9ab40c84d9299701366235d0e46
Kaporos <theo@daron.be>
2022-06-10 14:01:47 +0200

    fixing typos

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.CONFIG = {
     //sib: "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",
     sib: import.meta.env.VITE_SIB,
-    sibEmail: import.meta.sibEmail.VITE_SIB_EMAIL,
-    sibName: import.meta.sibName.VITE_SIB_NAME,
+    sibEmail: import.meta.env.VITE_SIB_EMAIL,
+    sibName: import.meta.env.VITE_SIB_NAME,
     mqttHostname: "mqtt.eclipseprojects.io",
     mqttHttpPort: 80,
     mqttSslPort: 443,
