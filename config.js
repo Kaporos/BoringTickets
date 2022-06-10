1ac377931a36c9e13318a70e022d0086f66c23b6
Kaporos <theo@daron.be>
2022-06-10 14:05:18 +0200

    bad comment

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.CONFIG = {
-    //sib: "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",
     sib: import.meta.env.VITE_SIB,
     sibEmail: import.meta.env.VITE_SIB_EMAIL,
     sibName: import.meta.env.VITE_SIB_NAME,
